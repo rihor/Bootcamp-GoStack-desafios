a4cec73ea2410f5dab9fbe20311c323948e7c737
Pedro Pinho <rihor1000@gmail.com>
2019-06-27 14:31:20 -0300

    configuração de comunicação com o bd

---
+++ desafio02 - Meetapp/src/config/database.js
@@ -0,0 +1,15 @@
+/**
+ * configuração de comunicação com o banco de dados
+ */
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'meetapp',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
