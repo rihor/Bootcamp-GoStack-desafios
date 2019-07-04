336097638b9ada0f8f334f5270ed340891ae58ac
Pedro Pinho <rihor1000@gmail.com>
2019-07-04 11:58:28 -0300

    Concertado migration de usuário

--- desafio02 - Meetapp/src/database/migrations/20190627170756-create-users.js
+++ desafio02 - Meetapp/src/database/migrations/20190627170756-create-users.js
@@ -27,11 +27,6 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      provider: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
-        allowNull: false,
-      },
       created_at: { type: Sequelize.DATE, allowNull: false },
       updated_at: { type: Sequelize.DATE, allowNull: false },
     });
