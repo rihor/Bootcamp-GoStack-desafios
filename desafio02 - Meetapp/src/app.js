63b1be82c15f2d3a514a195c8c12088096ea1578
Pedro Pinho <rihor1000@gmail.com>
2019-07-05 16:52:43 -0300

    Adicionando Sentry ao app

--- desafio02 - Meetapp/src/app.js
+++ desafio02 - Meetapp/src/app.js
@@ -1,7 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
 import { resolve } from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 
+import sentryConfig from './config/sentry';
 import routes from './routes';
 import './database';
 
@@ -9,11 +13,15 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     // permite uso de json nas rotas
     this.server.use(express.json());
     this.server.use(express.urlencoded({ extended: false }));
@@ -26,6 +34,18 @@ class App {
 
   routes() {
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
