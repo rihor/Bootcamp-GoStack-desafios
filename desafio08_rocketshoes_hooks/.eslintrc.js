ce75d3f83c3cf16f0e048775ab92a52bd926c8ed
Pedro Pinho <rihor1000@gmail.com>
2019-07-19 17:29:13 -0300

    Adicionando regra no ESLint

--- desafio08_rocketshoes_hooks/.eslintrc.js
+++ desafio08_rocketshoes_hooks/.eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
         extensions: ['.jsx', '.js']
       }
     ],
-    'import/prefer-default-export': 'off'
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off'
   }
 };
