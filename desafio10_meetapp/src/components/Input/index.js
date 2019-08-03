24bc86fc7b70c7892de2a0615baa49bc49dd770d
Pedro Pinho <rihor1000@gmail.com>
2019-08-03 17:41:55 -0300

    Adaptado o Input para aceitar icones do MaterialCommunityIcons Adicionado exemplo no começo do arquivo

--- desafio10_meetapp/src/components/Input/index.js
+++ desafio10_meetapp/src/components/Input/index.js
@@ -1,5 +1,18 @@
+/**
+ * Exemplo simples de uso:
+ * <Input
+ *    icon={{ type: 'community', name: 'lock-question' }}
+ *    placeholder="Confirme sua nova senha"
+ * />
+ *
+ * Ou
+ *
+ * <FormInput icon="person-outline" placeholder="Nome" />
+ */
+
 import React, { forwardRef } from 'react';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import MaterialComunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import PropTypes from 'prop-types';
 
 import { Container, TInput } from './styles';
@@ -7,14 +20,29 @@ import { Container, TInput } from './styles';
 function Input({ style, icon, ...rest }, ref) {
   return (
     <Container style={style}>
-      {icon && <Icon name={icon} size={20} color="rgba(255,255,255,0.6)" />}
+      {icon && icon.type && icon.type === 'community' && (
+        <MaterialComunityIcon
+          name={icon.name}
+          size={20}
+          color="rgba(255,255,255,0.6)"
+        />
+      )}
+      {typeof icon === 'string' && (
+        <MaterialIcon name={icon} size={20} color="rgba(255,255,255,0.6)" />
+      )}
       <TInput {...rest} ref={ref} />
     </Container>
   );
 }
 
 Input.propTypes = {
-  icon: PropTypes.string,
+  icon: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape({
+      type: PropTypes.oneOf(['community']).isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  ]),
   style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
 };
 
