2ff816d6b5a25605cb256839bc12dce4796b2a0d
Pedro Pinho <rihor1000@gmail.com>
2019-08-03 11:37:03 -0300

    Instalando react-native-gesture-handler

--- desafio10_meetapp/android/app/src/main/java/com/desafio10_meetapp/MainActivity.java
+++ desafio10_meetapp/android/app/src/main/java/com/desafio10_meetapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.desafio10_meetapp;
 
 import com.facebook.react.ReactActivity;
+// react-native-gesture-handler
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+// react-native-gesture-handler
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +17,16 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "desafio10_meetapp";
     }
+
+    // react-native-gesture-handler
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+         return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
+    }
+    // react-native-gesture-handler
 }
