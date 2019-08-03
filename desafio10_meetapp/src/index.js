65b686c05469032ca472309663a9c3b470f485e0
Pedro Pinho <rihor1000@gmail.com>
2019-08-03 13:23:33 -0300

    Mudada a cor da StatusBar Agora combina com a Header

--- desafio10_meetapp/src/index.js
+++ desafio10_meetapp/src/index.js
@@ -11,7 +11,7 @@ export default function Index() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <StatusBar barStyle="light-content" backgroundColor="#000" />
+        <StatusBar barStyle="light-content" backgroundColor="#18161f" />
         <App />
       </PersistGate>
     </Provider>
